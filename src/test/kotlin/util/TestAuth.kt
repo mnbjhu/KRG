f1c75e2662826a199a72139ecb9c08e2a900da64
james <james-gibson@ntlworld.com>
2022-11-28 19:57:39 +0000

    setting test defaults to localhost:6379

--- src/test/kotlin/util/TestAuth.kt
+++ src/test/kotlin/util/TestAuth.kt
@@ -10,9 +10,7 @@ object TestAuth {
     private val server_port = Key("server.port", intType)
     private val server_host = Key("server.host", stringType)
     private val server_pass = Key("server.pass", stringType)
-    val host = config[server_host]
-    val port = config[server_port]
-    val password = try{ config[server_pass] }catch (e: Exception) {
-        null
-    }
+    val host = try{ config[server_host] } catch (e: Exception) { "localhost" }
+    val port = try{ config[server_port] } catch (e: Exception) { 6379 }
+    val password = try{ config[server_pass] } catch (e: Exception) { null }
 }
